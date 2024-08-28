fd3bb6fc98a85cc18491154580b1588121683178
MuhammadHuzaifaRauf <mhuzaifarauf13@gmail.com>
2024-08-28 18:25:53 +0500

    feat: make code more readable

--- app/(components)/socialLinks/SocialLinks.tsx
+++ app/(components)/socialLinks/SocialLinks.tsx
@@ -1,50 +1,45 @@
-import React from 'react'
+import { socialLinks } from '@/app/utils/helpers';
+import React from 'react';
 
 export default function SocialLinks() {
+
   return (
     <article className="flex flex-col gap-8">
-    <header className="flex w-full flex-row justify-between gap-2">
-      <h3 className="text-lg text-neutral-100">Get in touch</h3>
-    </header>
-    <p>
-      Email me at{" "}
-      <a
-        href="mailto:mhuzaifarauf13@gmail.com"
-        className="hover:text-neutral-100 cursor-pointer"
-        rel="noreferrer"
-        target="_blank"
-      >
-       mhuzaifarauf13@gmail.com
-        <span className="sr-only">mhuzaifarauf13@gmail.comlink</span>
-      </a>{" "}
-      or follow me via my social links.
-    </p>
-
-    <ul role="list" className="flex flex-row gap-2">
-      <li>
+      <header className="flex w-full flex-row justify-between gap-2">
+        <h3 className="text-lg text-neutral-100">Get in touch</h3>
+      </header>
+      <p>
+        Email me at{" "}
         <a
-          href="https://www.linkedin.com/in/muhammad-huzaifa-rauf-b5ba032a7/"
+          href="mailto:mhuzaifarauf13@gmail.com"
           className="hover:text-neutral-100 cursor-pointer"
           rel="noreferrer"
           target="_blank"
         >
-          LinkedIn<span className="sr-only">X link</span>
-        </a>
-      </li>
+          mhuzaifarauf13@gmail.com
+          <span className="sr-only">mhuzaifarauf13@gmail.com link</span>
+        </a>{" "}
+        or follow me via my social links.
+      </p>
 
-      <li>/</li>
-      <li>
-        <a
-          href="https://github.com/MuhammadHuzaifaRau"
-          className="hover:text-neutral-100 cursor-pointer"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Github
-          <span className="sr-only">Github link</span>
-        </a>
-      </li>
-    </ul>
-  </article>
-  )
+      <ul role="list" className="flex flex-row gap-2">
+        {socialLinks.map((link, index) => (
+          <React.Fragment key={index}>
+            <li>
+              <a
+                href={link.url}
+                className="hover:text-neutral-100 cursor-pointer"
+                rel="noreferrer"
+                target="_blank"
+              >
+                {link.name}
+                <span className="sr-only">{`${link.name} link`}</span>
+              </a>
+            </li>
+            {index < socialLinks.length - 1 && <li>/</li>}
+          </React.Fragment>
+        ))}
+      </ul>
+    </article>
+  );
 }
--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -2,3 +2,15 @@ export const links = [
     { href: "#home", label: "Home" },
     { href: "#about", label: "About" }
   ]
+
+  export const socialLinks = [
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/muhammad-huzaifa-rauf-b5ba032a7/",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/MuhammadHuzaifaRau",
+    },
+  ];
+
